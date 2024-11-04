98ea8ff1e03e3797032112c439e3153d52c1117a
belong <coder-belong@foxmail.com>
2024-11-04 17:28:00 +0800

    fix: 更新

---
+++ test.js
@@ -0,0 +1,7 @@
+const obj = {
+  "PVQRdCxbpkbJkIXS2CUvtyHtyRWfn0td":
+    '{"cookie":{"originalMaxAge":604800000,"expires":"2024-11-11T08:40:55.654Z","secure":false,"httpOnly":true,"path":"/"},"userId":2}',
+  "JHsbdXr2e-e1tRI5FvmAxGKxCHqer6Gx":
+    '{"cookie":{"originalMaxAge":604800000,"expires":"2024-11-11T08:41:18.367Z","secure":false,"httpOnly":true,"path":"/"},"userId":1}',
+};
+console.log(obj["undefined"]);
